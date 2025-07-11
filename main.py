b8bfcdf029ba296b120562a5c4996fcc48cb3987
King-knight163 <crunchfun885@gmail.com>
2025-07-11 15:17:38 +0530

    main.py

--- main.py
+++ main.py
@@ -216,6 +216,13 @@ app = Flask(__name__)
 def index():
     return "Bot is running", 200
 
+app = Flask(__name__)
+@app.route('/')
+def home(): return "Bot is Running!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)
+
 if __name__ == "__main__":
-    threading.Thread(target=lambda: app.run(host="0.0.0.0", port=8000)).start()
-    main()  # Make sure `main()` function is defined above
+    threading.Thread(target=run_flask).start()
+    main()
